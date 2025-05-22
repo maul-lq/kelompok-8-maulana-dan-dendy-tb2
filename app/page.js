2b2902769a09265e149178039632a907078534f0
Rizlrad Fz <rizlradfz@gmail.com>
2025-05-22 21:53:35 +0700

    temp

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="/tugas-besar-2/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -29,7 +29,7 @@ export default function Home() {
           >
             <Image
               className={styles.logo}
-              src="/vercel.svg"
+              src="/tugas-besar-2/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -54,7 +54,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="/tugas-besar-2/file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -68,7 +68,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="/tugas-besar-2/window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -82,7 +82,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src="/tugas-besar-2/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
